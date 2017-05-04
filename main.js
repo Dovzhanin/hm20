abcb3514e738310ef4160ea3535e4647c7134f45
Veronika <dovzhaninveronika@gmail.com>
2017-05-04 21:56:54 +0300

    n

---
+++ main.js
@@ -0,0 +1,19 @@
+var mynames = prompt('Enter your name');
+console.log('Your name is', mynames);
+var x = Number(prompt('enter x'));
+var y = Number(prompt('enter y'));
+var z = Number(prompt('enter z'));
+var formyle = x + (y*z);
+prompt(formyle);
+var firstnumber = Number(prompt('enter first number'));
+var secondnumber = Number(prompt('enter second number'));
+var resadd = firstnumber - secondnumber;
+var ressub = firstnumber + secondnumber;
+var resmult = firstnumber * secondnumber;
+var resdiv = firstnumber / secondnumber;
+var resmov = firstnumber % secondnumber;
+console.log('add',resadd)
+console.log('sub',ressub)
+console.log('mult',resmult)
+console.log('div',resdiv)
+console.log('mov',resmov)
